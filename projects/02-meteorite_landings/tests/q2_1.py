dff874c0ffc530ab0323ffdf994f8fc3193a2340
nixrunner <nixrunner@localhost>
2021-07-27 17:56:23 +0000

    Machine user push.

--- projects/02-meteorite_landings/tests/q2_1.py
+++ projects/02-meteorite_landings/tests/q2_1.py
@@ -1,3 +1,1 @@
-test = {   'name': 'q2_1',
-    'points': 1,
-    'suites': [{'cases': [{'code': '>>> not_plausible in {1, 2, 3, 4}\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q2_1', 'points': 1, 'suites': [{'cases': [{'code': '>>> 0 <= prop_1950 <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
