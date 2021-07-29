4f64fcf967c9e13133a8cdfd1a1274e2d3de7b25
nixrunner <nixrunner@localhost>
2021-07-29 19:16:12 +0000

    Machine user push.

--- projects/02-meteorite_landings/tests/q3_15.py
+++ projects/02-meteorite_landings/tests/q3_15.py
@@ -1,7 +1,1 @@
-test = {   'name': 'q3_15',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> len(asia_ci)\n2', 'hidden': False, 'locked': False}, {'code': '>>> len(na_ci)\n2', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
+test = {'name': 'q3_15', 'points': 1, 'suites': [{'cases': [{'code': '>>> 0 <= perm_p_value <= 1\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
